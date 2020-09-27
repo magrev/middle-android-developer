64930ab0ecc4ce46aa4c107c047b6fa68269e6e3
Revner <magrev@yandex.ru>
2020-09-27 18:18:52 +0500

    hometask_4

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -4,15 +4,15 @@ package ru.skillbranch.skillarticles.extensions
  * Created by Reva on 07.09.2020
  */
 
-fun String?.indexesOf(query: String): List<Int> {
-    if (query == null || query.isEmpty()) {
+fun String?.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int> {
+    if (substr == null || substr.isEmpty()) {
         return emptyList()
     }
     var fromIndex = 0
     val list = ArrayList<Int>()
     return if (this != null) {
-        while (this.indexOf(query, fromIndex, ignoreCase = true) > -1) {
-            fromIndex = this.indexOf(query, fromIndex, ignoreCase = true)
+        while (this.indexOf(substr, fromIndex, ignoreCase = ignoreCase) > -1) {
+            fromIndex = this.indexOf(substr, fromIndex, ignoreCase = ignoreCase)
             list.add(fromIndex)
             println("Found at => $fromIndex")
             fromIndex++
